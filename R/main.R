cc1463b2e4c67b8d38e1f55962a712ba9e44a08b
Kyle Andrews <kyle.c.andrews@gmail.com>
2018-12-10 23:59:42 -0500

    Added my first draft.

---
+++ R/main.R
@@ -0,0 +1,105 @@
+
+flatmap <- function(xs, fun) {
+  unlist(lapply(xs, fun),
+         use.names = FALSE,
+         recursive = FALSE)
+}
+
+#' Build a custom compound operator.
+#' @param sep Character(1) operator name.
+#' @param prefix Logical(1) should operator be at beginning?
+#' @param group Logical(1) should the parts be wrapped in parens?
+#' @param max_length Integer(1) maximum number of named arguments.
+#' @return Function -> Character
+#' @name Compound
+#' @export
+compound <- function(sep, prefix = TRUE, group = TRUE, max_length = Inf) {
+  stopifnot(length(sep) == 1)
+  sep = trimws(sep)
+  sep = paste0(" -", sep, " ")
+  function(...) {
+    find_args = list(...)
+    stopifnot(length(find_args) <= max_length)
+    arg_name = names(find_args)
+    map = flatmap(arg_name, function(nm) {
+      paste(paste0("-", nm),
+            shQuote(find_args[[nm]]),
+            collapse = sep)
+    })
+    out = paste0(map, collapse = sep)
+    grouped = grepl(sep, out) && group
+    # cat("Prefixed:", prefix, "\nGrouped:", grouped, "\n")
+    if(prefix && !grouped) {
+      trimws(paste0(sep, out))
+    } else if(prefix && grouped) {
+      paste("\\(", trimws(paste0(sep, out)), "\\)")
+    } else if(!prefix && grouped) {
+      paste("\\(", out, "\\)")
+    } else {
+      out
+    }
+  }
+}
+
+#' @rdname Compound
+#' @export
+or <- compound("or")
+
+#' @rdname Compound
+#' @export
+and <- compound("and")
+
+#' @rdname Compound
+#' @export
+not <- compound("not", max_length = 1)
+
+
+#' Wrapper around GNU Find CLI utility.
+#'
+#' It's designed so that arguments are just passed in
+#' order much like the GNU Find utility itself.
+#'
+#' Hopefully this will be somewhat useful to someone.
+#'
+#' There are two special kinds of argument names that may be repeated:
+#'
+#' - switch (sw) :: switches which don't have arguments
+#' - compound (cmp) :: compound statements
+#'
+#' These enable this wrapper to handle the a broader chunch of the
+#' Find utilities functionality.
+#'
+#' Please refer the GNU Find INFO pages for more details.
+#' @param search_path Character path names.
+#' @param ... Arguments to find.
+#' @param exec Character(1) path to GNU Find executable.
+#' @param debug Logical(1) whether to print generated command.
+#' @return Character.
+#' @examples
+#' # Find all non-hidden directories in $HOME.
+#' gnu_find(search_path = "~",
+#'          type = "d", maxdepth = 1,
+#'          compound = not(path = "*/\\.*"))
+#' @export
+gnu_find <- function(search_path = getwd(),
+                     ...,
+                     exec = "find",
+                     debug = FALSE) {
+  find_args = c(...)
+  argname = names(find_args)
+  special  = c("~", ".")
+  switch = argname %in% c("sw", "switch")
+  compound = argname %in% c("cmp", "compound")
+  pathstr = c(ifelse(search_path %in% special, search_path, shQuote(path)))
+  instr = ifelse(switch, paste0("-", find_args),
+                 ifelse(compound, find_args,
+                        paste(paste0("-", argname),
+                              shQuote(unname(find_args)))))
+  input = trimws(c(pathstr, instr))
+  if(debug) {
+    cat("Find command:\n",
+        paste(exec, paste(input, collapse = " ")),
+        "\n")
+  }
+  system2(command = exec, args = input, stdout = TRUE)
+}
